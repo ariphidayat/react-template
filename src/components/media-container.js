9fa8fcce9b330e32b43d4f727eb39f72bb0907d9
Arip Hidayat <ariphidayat@hotmail.com>
2017-08-24 07:57:23 +0700

    css optimized

--- src/components/media-container.js
+++ src/components/media-container.js
@@ -4,17 +4,17 @@ import arip from '../images/arip.jpg';
 export default class MediaContainer extends Component {
   render() {
     return (
-      <div className="col-md-7">
-        <ul className="media-container">
-          <li className="media">
-            <img className="d-flex mr-3" src={arip} width="64" alt="Generic placeholder image"/>
+      <section className="col-md-7">
+        <div className="media-container">
+          <div className="media">
+            <img className="d-flex mr-3" src={arip} width="64" alt="Image placeholder"/>
             <div className="media-body">
-              <h5 className="mt-0 mb-1">Media Object</h5>
+              <h5 className="mt-0 mb-1">Subject</h5>
               Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
             </div>
-          </li>
-        </ul>
-      </div>
+          </div>
+        </div>
+      </section>
     )
   }
 }
