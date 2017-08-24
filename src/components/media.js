220546b88e46cb13c86441b4997233e02a4520f5
Arip Hidayat <ariphidayat@hotmail.com>
2017-08-24 12:47:03 +0700

    destructure 'props' object

--- src/components/media.js
+++ src/components/media.js
@@ -1,11 +1,11 @@
 import React from 'react';
 
-export const Media = (props) => (
+export const Media = ({img, subject, body}) => (
       <div className="media">
-        <img className="d-flex mr-3" src={props.img} width="64" alt="Image placeholder"/>
+        <img className="d-flex mr-3" src={img} width="64" alt="Image placeholder"/>
         <div className="media-body">
-          <h5 className="mt-0 mb-1">{props.subject}</h5>
-          <p>{props.body}</p>
+          <h5 className="mt-0 mb-1">{subject}</h5>
+          <p>{body}</p>
         </div>
       </div>
 )
