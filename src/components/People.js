587d7c3d806fecdb8967ab6e92514a610bd6b1f6
Arip Hidayat <ariphidayat@hotmail.com>
2017-09-16 04:53:45 +0700

    tidy

--- src/components/People.js
+++ src/components/People.js
@@ -8,12 +8,15 @@ class People extends Component {
   }
 
   renderPeople(people, i) {
+    let name = people.name.length > 15 ? people.name.substring(0,14).concat('..') : people.name
+    let email = people.email.length > 25 ? people.email.substring(0,25).concat('..') : people.email
+
     return (
       <div key={i} className="card">
         <img className="card-img-top" src="http://via.placeholder.com/204x204"/>
         <div className="card-body">
-          <h5 className="card-title">{people.name}</h5>
-          <p>{people.email}</p>
+          <h5 className="card-title">{name}</h5>
+          <p className="card-text"><small>{email}</small></p>
           <a href="#" className="btn btn-primary">Follow</a>
         </div>
       </div>
