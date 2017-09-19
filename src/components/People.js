1b0cb9fc08e5a4ca402cdd8a5de4ec768184ca51
Arip Hidayat <ariphidayat@hotmail.com>
2017-09-19 22:09:37 +0700

    refactor to more readable code

--- src/components/People.js
+++ src/components/People.js
@@ -8,29 +8,33 @@ class People extends Component {
     this.props.fetchPeople()
   }
 
-  renderPeople(people, i) {
-    let name = people.name.length > 15 ? people.name.substring(0,14).concat('..') : people.name
-    let email = people.email.length > 25 ? people.email.substring(0,25).concat('..') : people.email
+  renderPeople() {
+    return this.props.peoples.map((people, i) => {
+      let name = people.name.length > 15 ? people.name.substring(0,14).concat('..') : people.name
+      let email = people.email.length > 25 ? people.email.substring(0,25).concat('..') : people.email
 
-    return (
-      <div key={i} className="card">
-        <img className="card-img-top" src="http://via.placeholder.com/204x204"/>
-        <div className="card-body">
-          <h5 className="card-title">
-            <Link to={`/people/${people.id}`}>{name}</Link>
-          </h5>
-          <p className="card-text"><small>{email}</small></p>
-          <a href="#" className="btn btn-primary">Follow</a>
+      return (
+        <div key={i} className="card">
+          <img className="card-img-top" src="http://via.placeholder.com/204x204"/>
+          <div className="card-body">
+            <h5 className="card-title">
+              <Link to={`/people/${people.id}`}>{name}</Link>
+            </h5>
+            <p className="card-text">
+              <small>{email}</small>
+            </p>
+            <a href="#" className="btn btn-primary">Follow</a>
+          </div>
         </div>
-      </div>
-    )
+      )
+    })
   }
 
   render() {
     return (
       <section className="col-md-7">
         <div className="row">
-          {this.props.peoples.map((p, i) => this.renderPeople(p, i))}
+          {this.renderPeople()}
         </div>
       </section>
     )
